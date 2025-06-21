c337bb5cc0550d5aafd18e444e4c8cdcfd89a9c2
chris <christoph21.schweiger@gmx.de>
2025-06-21 22:57:03 +0200

    method docstrings

--- api/app.py
+++ api/app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route("/hi")
 def calling():
+    """ This method sends response via json to the frontend """
     return {'message': "hi"}
 
 
 def calculate_sum(a, b):
+    """ This method calculates the sum of two numbers and returns the value """
     return a + b
